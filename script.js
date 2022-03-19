15b2bf49e47f3a8a5e1b0b17f61c77a36c5c99f3
Bono-K <71620264+Bono-K@users.noreply.github.com>
2022-03-19 18:47:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13382,7 +13382,7 @@ function shakeTiles(tiles) {
 
 function checkWinLose(guess, tiles) {
   if (guess === targetWord) {
-    showAlert("Great job!\n You did it!", 5000)
+    showAlert("Great job! You got it!", 5000)
     showAlert(winMessage, 5000)
     danceTiles(tiles)
     stopInteraction()
@@ -13392,6 +13392,7 @@ function checkWinLose(guess, tiles) {
   const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])")
   if (remainingTiles.length === 0) {
     showAlert(targetWord.toUpperCase(), null)
+    showAlert(winMessage, 5000)
     stopInteraction()
   }
 }

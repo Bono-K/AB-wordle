d1c4685c94768c399719b33e6305418ac84b30d5
Bono-K <71620264+Bono-K@users.noreply.github.com>
2022-03-21 18:42:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13282,6 +13282,7 @@ function submitGuess() {
 		// The letter is correct
 		if (targetWord[i] === guess[i]) {
 			classDictionary[i + 1] = 'correct'
+			activeNoLetterTiles.dataset.state[i] = 'correct'
 
 			matchingChars = matchingChars + letter
 			continue

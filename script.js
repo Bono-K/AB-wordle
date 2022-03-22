9c3c3a3833bac900b4baade98df18868a6303efb
Bono-K <71620264+Bono-K@users.noreply.github.com>
2022-03-21 20:02:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13339,7 +13339,7 @@ function submitGuess() {
 function flipTile(tile, index, array, guess, className) {
 	const letter = tile.dataset.letter
 	const key = keyboard.querySelector(`[data-key="${letter}"i]`)
-//	const activeNoLetterTiles = [...getActiveNoLetterTiles()]
+	const activeNoLetterTiles = [...getActiveNoLetterTiles()]
 	
 	setTimeout(() => {
 		tile.classList.add('flip')
@@ -13354,7 +13354,7 @@ function flipTile(tile, index, array, guess, className) {
 
 			tile.dataset.state = className
 			key.classList.add(className)
-//			activeNoLetterTiles.dataset.state = classname
+			activeNoLetterTiles.dataset.state = classname
 
 			if (index === array.length - 1) {
 				tile.addEventListener(
